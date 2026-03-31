0ff9af0d7a7ac7716f38bbb9d8dcbb776b0e0fd2
Mikhail Romanovskiy <romanovskymv@gmail.com>
2026-03-31 13:11:21 +0300

    Remove dead code

--- src/CSV/CSV.h
+++ src/CSV/CSV.h
@@ -1,8 +1,4 @@
 #pragma once
 #include <stdbool.h>
 
-// static void countColumnsAndRows(const char* inp, int* rows, int* columns);
-// static int* makeArrayOfWidth(const char* inp, int rows, int columns);
-// static void printRow(FILE* out, char* buffer, const int* widths, int columns, bool heading);
-// static void printRows(const char* inp, const char* out, int* widths, int rows, int columns);
 bool prettyPrinter(const char* inp, const char* out);
